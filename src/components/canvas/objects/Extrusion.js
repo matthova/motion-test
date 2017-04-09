1a03642d90be9c02a7c06b9c9029a3b0f1eebc06
matthova <matthova87@gmail.com>
2017-04-09 16:23:53 -0700

    dump implementation of gcode viewer working

--- src/components/canvas/objects/Extrusion.js
+++ src/components/canvas/objects/Extrusion.js
@@ -1,44 +1,78 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import { gcodeToObject } from 'gcode-json-converter';
 
+import gcode from './gcode';
 export default class Extrusion extends Component {
+
+  // naïve implementation of extrusion profile
+  createExtrusionProfile() {
+    const extrusionWidth = 0.4;
+    const layerHeight = 0.3;
+    const verticies = [];
+    verticies.push(new THREE.Vector2(-extrusionWidth / 2, layerHeight / 2));
+    verticies.push(new THREE.Vector2(-extrusionWidth / 2 + layerHeight / 2, layerHeight));
+    verticies.push(new THREE.Vector2(extrusionWidth / 2 - layerHeight / 2, layerHeight));
+    verticies.push(new THREE.Vector2(extrusionWidth / 2, layerHeight / 2));
+    verticies.push(new THREE.Vector2(extrusionWidth / 2 - layerHeight / 2, 0));
+    verticies.push(new THREE.Vector2(-extrusionWidth/2 + layerHeight / 2, 0));
+    return verticies;
+  }
+
   render() {
-    const extrudeSettings = {
-      amount: 8,
-      bevelEnabled: true,
-      bevelSegments: 2,
-      steps: 2,
-      bevelSize: 1,
-      bevelThickness: 1,
+    const gcodeArray = gcode.split('\n');
+    const objects = gcodeArray.map(gcode => gcodeToObject(gcode));
+
+    const currentPosition = {
+      x: 0,
+      y: 0,
+      z: 0
     };
 
-    const x = 0;
-    const y = 0;
-    const z = 0;
-    const rotation = new THREE.Euler(0, 0, 0);
-    const scale = new THREE.Vector3(1, 1, 1);
+    let lastPoint = new THREE.Vector3(currentPosition.x, currentPosition.y, currentPosition.z);
 
-    const verticies = [];
-    verticies.push(new THREE.Vector2(0, 0));
-    verticies.push(new THREE.Vector2(10, 0));
-    verticies.push(new THREE.Vector2(0, 10));
+    const extrusions = [];
+    objects.forEach((gcode, i) => {
+      let start;
+      let end;
+      let path;
+      if (gcode.args.e != undefined) {
+        path = new THREE.CurvePath();
+        start = Object.assign({}, lastPoint);
+      }
+
+      currentPosition.x = objects[i].args.x || currentPosition.x;
+      currentPosition.y = objects[i].args.y || currentPosition.y;
+      currentPosition.z = objects[i].args.z || currentPosition.z;
+      lastPoint = new THREE.Vector3(currentPosition.x, currentPosition.y, currentPosition.z);
+
+      if (gcode.args.e != undefined) {
+        const line = new THREE.LineCurve3(start, lastPoint);
+        path.add(line);
+        const extrudeSettings = {
+          amount: 8,
+          bevelEnabled: true,
+          bevelSegments: 2,
+          steps: 2,
+          bevelSize: 1,
+          bevelThickness: 1,
+          extrudePath: path,
+        };
+        extrusions.push(
+          <mesh key={i} rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}>
+            <extrudeGeometry settings={extrudeSettings}>
+              <shape points={this.createExtrusionProfile()} />
+            </extrudeGeometry>
+            <meshPhongMaterial color={0xff0000} />
+          </mesh>
+        );
+      }
+    });
 
     return (
-      <mesh
-        // 3d shape
-        position={new THREE.Vector3(x, y, z - 75)}
-        rotation={rotation}
-        scale={scale}
-      >
-        <extrudeGeometry
-          settings={extrudeSettings}
-        >
-          <shape points={verticies}/>
-        </extrudeGeometry>
-        <meshBasicMaterial
-          color={0xff0000}
-        />
-      </mesh>
+      <group>
+        {extrusions}
+      </group>
     );
   }
 };
