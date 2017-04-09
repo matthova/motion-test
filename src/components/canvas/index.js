641f7ae9b5fd82571fca7ca6ecd888ce9ea3b65f
matthova <matthova87@gmail.com>
2017-04-09 16:23:33 -0700

    changed camera view and added light

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -12,7 +12,7 @@ class Canvas extends Component {
 
     // construct the position vector here, because if we use 'new' within render,
     // React will think that things have changed when they have not.
-    this.cameraPosition = new THREE.Vector3(0, 0, 5);
+    this.cameraPosition = new THREE.Vector3(0, 0, 20);
 
     this._onAnimate = () => {
       // we will get this callback every frame
@@ -39,18 +39,27 @@ class Canvas extends Component {
         width={width}
         height={height}
         onAnimate={this._onAnimate}
+        antialias
+        pixelRatio={window.devicePixelRatio}
+        clearColor={0xf0f0f0}
       >
         <scene>
           <perspectiveCamera
             name="camera"
             ref="camera"
-            fov={75}
+            fov={50}
             aspect={width / height}
-            near={0.1}
+            near={.1}
             far={1000}
             position={this.cameraPosition}
-          />
+          >
+            <pointLight
+              color={0xffffff}
+              intensity={0.5}
+            />
+          </perspectiveCamera>
           <Extrusion/>
+          <gridHelper size={100}/>
         </scene>
       </React3>
     );
